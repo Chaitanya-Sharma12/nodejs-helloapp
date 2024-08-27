953a6a751d7e098e765505aba4405bcc56406ed2
Chaitanya-Sharma12 <166804490+Chaitanya-Sharma12@users.noreply.github.com>
2024-08-27 13:18:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('HI THIS IS CHAITANYA SHARMA Appinventiv and node deploy in two different server'); //write a response to the client
+  res.write('HI THIS IS CHAITANYA SHARMA Appinventiv and node deploy in two different serve lets check'); //write a response to the client
   res.end(); //end the response
 }).listen(3000); //the server object listens on port 80 , mention your port specific
 
