97db44ab027bf175ee5b97776634f415f6eb31f5
Chaitanya-Sharma12 <166804490+Chaitanya-Sharma12@users.noreply.github.com>
2024-09-07 17:48:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('HI THIS IS CHAITANYA SHARMA '); //write a response to the client
+  res.write('Hello World'); //write a response to the client
   res.end(); //end the response
 }).listen(3030); //the server object listens on port 80 , mention your port specific
 
